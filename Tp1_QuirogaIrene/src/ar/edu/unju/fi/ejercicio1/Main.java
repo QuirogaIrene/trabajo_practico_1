b10a396ee7539e8c5084f620d34223b5d3ee897b
Familia <Familia@DESKTOP-8GMHG6N>
2024-04-05 22:39:02 -0300

    Ejercicio 1 correspondiente al Tp1 de Programacion Visual terminado.

---
+++ Tp1_QuirogaIrene/src/ar/edu/unju/fi/ejercicio1/Main.java
@@ -0,0 +1,22 @@
+package ar.edu.unju.fi.ejercicio1;
+
+public class Main {
+
+	public static void main(String[] args) {
+		//declaracion de las variables enteras.
+		int a = 8;
+		int b = 5;
+		int c = 1;
+		int d = 20;
+		int e = 9;
+		
+		//declaracion de la variable promedio y asignacion del calculo promedio de las variables.
+		float promedioFloat = (float)(a+b+c+d+e)/5 ;
+		
+		//Mostramos por pantalla/consola el resultado almacenado en la variable promedioDouble.
+		System.out.println("El promedio de las variables es: "+ promedioFloat);
+		
+
+	}
+
+}
