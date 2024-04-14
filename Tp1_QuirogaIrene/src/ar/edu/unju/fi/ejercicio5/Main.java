e415a34e904417b0a267f2bcb69432c53bd7a918
chechar <cesar@openix.com.ar>
2024-04-14 20:36:48 -0300

    Agregado del punto 5

---
+++ Tp1_QuirogaIrene/src/ar/edu/unju/fi/ejercicio5/Main.java
@@ -0,0 +1,25 @@
+package ar.edu.unju.fi.ejercicio5;
+
+import java.util.Iterator;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner scanner=new Scanner(System.in);
+		int resultado=0;
+		System.out.println("Ingrese un numero");
+		int num = scanner.nextInt();
+			for (int i = 1; i <=10 && (num>=1 && num<=9) ; i++) {
+				resultado=num*i;
+				System.out.println(num+"X"+i+"="+resultado);
+				
+			}
+
+		scanner.close();	
+		}
+
+	}
+
+
