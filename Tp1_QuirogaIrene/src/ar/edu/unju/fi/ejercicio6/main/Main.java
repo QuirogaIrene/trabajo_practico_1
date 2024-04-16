f2f9e13345db5ed79c65d442b9543215ffa0fdc3
chechar <cesar@openix.com.ar>
2024-04-16 02:38:19 -0300

    correccion de errores del punto 6

--- Tp1_QuirogaIrene/src/ar/edu/unju/fi/ejercicio6/main/Main.java
+++ Tp1_QuirogaIrene/src/ar/edu/unju/fi/ejercicio6/main/Main.java
@@ -1,24 +1,57 @@
 package ar.edu.unju.fi.ejercicio6.main;
 
+import java.time.LocalDate;
 import java.util.Scanner;
-
 import ar.edu.unju.fi.ejercicio6.model.Persona;
 
 public class Main {
 
 	public static void main(String[] args) {
-		Scanner scanner=new Scanner(System.in);
-		System.out.println("Ingrese Nombre:");
-		String nombre = scanner.next();
-		System.out.println("Ingrese Dni:");
-		String dni = scanner.next();
-		System.out.println("Ingrese Fecha de Nacimiento (AAAA-MM-DD):");
-		String fechaNacimiento = scanner.next();
-		Persona persona1 =new Persona();
-		System.out.println(persona1);
-	
-		
+		 Scanner scanner = new Scanner(System.in);
 
-	}
+	        //Creamos un objeto para utilizar el constructor por defecto
+	        Persona persona1 = new Persona();
+	        System.out.println("Ingrese el DNI de la 1ra persona:");
+	        persona1.setDni(scanner.nextLine());
+	        System.out.println("Ingrese el nombre de la 1ra persona:");
+	        persona1.setNombre(scanner.nextLine());
+	        System.out.println("Ingrese la fecha de nacimiento de la 1ra persona en formato(YYYY-MM-DD):");
+	        persona1.setFechaNacimiento(LocalDate.parse(scanner.nextLine()));
+	        System.out.println("Ingrese la provincia:");
+	        persona1.setProvincia(scanner.nextLine());
+
+	        System.out.println("Datos de la 1ra persona:");
+	        persona1.mostrarDatos();
+
+	        //Construcción del segundo objeto utilizando el constructor parametrizado
+	        System.out.println("Ingrese el DNI de la 2da persona:");
+	        String dni2 = scanner.nextLine();
+	        System.out.println("Ingrese el nombre de la 2da persona:");
+	        String nombre2 = scanner.nextLine();
+	        System.out.println("Ingrese la fecha de nacimiento de la 2da persona en formato (YYYY-MM-DD):");
+	        LocalDate fechaNacimiento2 = LocalDate.parse(scanner.nextLine());
+	        System.out.println("Ingrese la provincia para la Persona 2:");
+	        String provincia2 = scanner.nextLine();
+
+	        Persona persona2 = new Persona(dni2, nombre2, fechaNacimiento2, provincia2);
+
+	        System.out.println("Datos de la 2da persona:");
+	        persona2.mostrarDatos();
+
+	        // Construcción del 3er objeto utilizando el constructor con dni, nombre y fecha de nacimiento
+	        System.out.println("Ingrese el DNI de la 3ra persona:");
+	        String dni3 = scanner.nextLine();
+	        System.out.println("Ingrese el nombre de la 3da persona:");
+	        String nombre3 = scanner.nextLine();
+	        System.out.println("Ingrese la fecha de nacimiento de la 3ra persona en formato(YYYY-MM-DD):");
+	        LocalDate fechaNacimiento3 = LocalDate.parse(scanner.nextLine());
+
+	        Persona persona3 = new Persona(dni3, nombre3, fechaNacimiento3);
+
+	        System.out.println("Datos de la 3ra persona:");
+	        persona3.mostrarDatos();
+
+	        scanner.close();
+	    }
 
 }
--- Tp1_QuirogaIrene/src/ar/edu/unju/fi/ejercicio6/model/Persona.java
+++ Tp1_QuirogaIrene/src/ar/edu/unju/fi/ejercicio6/model/Persona.java
@@ -4,86 +4,83 @@ import java.time.LocalDate;
 import java.time.Period;
 
 public class Persona {
+    private String dni;
+    private String nombre;
+    private LocalDate fechaNacimiento;
+    private String provincia;
 
-	private String dni;
-	private String nombre;
-	private String fechaNacimientoStr;
-	private String provincia;
-	
-	public Persona() {
-	}
+    // Constructores de la clase persona
+    public Persona() {
+    }
 
-	public Persona(String dni, String nombre, String fechaNacimientoStr, String provincia) {
-		
-		this.dni = dni;
-		this.nombre = nombre;
-		this.fechaNacimientoStr = fechaNacimientoStr;
-		this.provincia = provincia;
-	}
+    public Persona(String dni, String nombre, LocalDate fechaNacimiento, String provincia) {
+        this.dni = dni;
+        this.nombre = nombre;
+        this.fechaNacimiento = fechaNacimiento;
+        this.provincia = provincia;
+    }
+    //aca ponemos a provincia "jujuy" 
+    public Persona(String dni, String nombre, LocalDate fechaNacimiento) {
+        this.dni = dni;
+        this.nombre = nombre;
+        this.fechaNacimiento = fechaNacimiento;
+        this.provincia = "Jujuy";
+    }
 
-	public Persona(String dni, String nombre, String fechaNacimientoStr) {
-		
-		this.dni = dni;
-		this.nombre = nombre;
-		this.fechaNacimientoStr = fechaNacimientoStr;
-		this.provincia = "Jujuy";
-	}
-	
-	
-	public String getDni() {
-		return dni;
-	}
+    //Metodos get and set
+    public String getDni() {
+        return dni;
+    }
 
-	public void setDni(String dni) {
-		this.dni = dni;
-	}
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
 
-	public String getNombre() {
-		return nombre;
-	}
+    public String getNombre() {
+        return nombre;
+    }
 
-	public void setNombre(String nombre) {
-		this.nombre = nombre;
-	}
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
 
-	public String getProvincia() {
-		return provincia;
-	}
+    public LocalDate getFechaNacimiento() {
+        return fechaNacimiento;
+    }
 
-	public void setProvincia(String provincia) {
-		this.provincia = provincia;
-	}
+    public void setFechaNacimiento(LocalDate fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
 
-	public LocalDate getFechaNacimiento() {
-		return fechaNacimiento;
-	}
+    public String getProvincia() {
+        return provincia;
+    }
 
-	public void setFechaNacimiento(LocalDate fechaNacimiento) {
-		this.fechaNacimiento = fechaNacimiento;
-	}
+    public void setProvincia(String provincia) {
+        this.provincia = provincia;
+    }
 
+    //Metodo para calcular la edad de la persona
+    public int calcularEdad() {
+        LocalDate fechaActual = LocalDate.now();
+        Period periodo = Period.between(this.fechaNacimiento, fechaActual);
+        return periodo.getYears();
+    }
+    //validamos que la persona sea mayor de edad y lo guardamos en un resultado logico
+    public boolean esMayorDeEdad() {
+        return calcularEdad() >= 18;
+    }
 
-	LocalDate fechaNacimiento = LocalDate.parse(fechaNacimientoStr);
-	
-	public int calcularEdad (LocalDate fechaNacimiento) {
-		
-		LocalDate fechaActual = LocalDate.now();
-		Period periodo = Period.between(fechaNacimiento, fechaActual);
-
-	return periodo.getYears();
-	}
-	
-	public void mostrarDatos(int periodo) {
-		String mayorEdadSrt;
-		if (periodo >=18 ) {
-			mayorEdadSrt=" y es mayor de edad";
-		}else
-		{
-			mayorEdadSrt=" y es menor de edad";
-		}
-		System.out.println("Nombre:"+this.nombre+" Dni:"+this.dni+" Fecha de Nacimiento:"+this.fechaNacimientoStr+" Provincia:"+this.provincia+" Edad:"+calcularEdad(this.fechaNacimiento)+mayorEdadSrt);
-	}
-
-	
-
+    public void mostrarDatos() {
+        System.out.println("DNI: " + dni);
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Fecha de Nacimiento: " + fechaNacimiento);
+        System.out.println("Provincia: " + provincia);
+        System.out.println("Edad: " + calcularEdad());
+        if (esMayorDeEdad()) {
+            System.out.println("La persona es mayor de edad");
+        } else {
+            System.out.println("La persona no es mayor de edad");
+        }
+    }
 }
